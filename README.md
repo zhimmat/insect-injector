465813370a08271fa10ddcfd2b088753e0bf9e41
Thomas J Bradley <hey@thomasjbradley.ca>
2016-02-29 20:18:51 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,30 @@
+# Insect injector
+
+- Approx. completion time: **1 hour**
+- Deliverables: **1 HTML file, 1 CSS file, 1 JS file, images**
+
+---
+
+**Quick project summary shown here.**
+
+- *Fork this repository.*
+- Convert the information found in `content.csv` into a data structure in the Javascript file.
+- All the information should be stored in a variable named `insects`
+- Use the function that outputs it as a reference to how the data should be structured.
+- **DO NOT change the HTML.**
+- **DO NOT change the CSS.**
+- *Run it through Markbot and make sure it passes all the checks.*
+
+---
+
+## Goal
+
+Visually match the images in the “screenshots” folder.
+
+- [Final screenshots in the “screenshots” folder.](screenshots)
+
+---
+
+## Hand in
+
+Drop this folder into your Markbot application. Make sure to fix all the errors. And submit to Canvas using Markbot.
---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Create your variable here, name it `insects`
+
+
+// ##########################################################
+// START DO NOT CHANGE ZONE
+// ##########################################################
+
+var writeInsects = function (bugs) {
+  document.write('<h1>Insects</h1>');
+
+  document.write('<ol>');
+
+  bugs.forEach(function (bug) {
+    document.write('<li>');
+    document.write('<h2>' + bug.name + '</h2>');
+    document.write('<img src="images/' + bug.img + '" alt="">');
+    document.write('<dl>');
+    document.write('<dt>Family</dt>');
+    document.write('<dd><i>' + bug.family + '</i></dd>');
+    document.write('<dt>Size</dt>');
+    document.write('<dd>' + bug.size[0] + '–' + bug.size[1] + ' mm</dd>');
+    document.write('<dt>Eats</dt>');
+
+    document.write('<dd>');
+
+    if (bug.food.eatsInsects) {
+      document.write('Insects');
+    }
+
+    if (bug.food.eatsDung) {
+      document.write('Dung');
+    }
+
+    if (bug.food.eatsPlants) {
+      document.write('Plants');
+    }
+
+    document.write('</dd>');
+
+    document.write('</dl>');
+    document.write('</li>');
+  });
+
+  document.write('</ol>');
+};
+
+writeInsects(insects);
+
+// ##########################################################
+// END DO NOT CHANGE ZONE
+// ##########################################################
