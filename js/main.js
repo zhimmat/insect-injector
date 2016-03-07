d0a7aade1e2b11211e86d5c5860e5682657d776f
zhimmat <zeyad.himmat@live.com>
2016-03-06 20:31:39 -0500

    fixed indentations in main.js

--- js/main.js
+++ js/main.js
@@ -32,16 +32,17 @@ var insects = [
       eatsDung: false,
       eatsPlants: true,
     }
-    {
-      name: 'Tortoise Beetle',
-      img: 'tortoise-beetle.jpg',
-      family: 'Cassidinae',
-      size: '1-18',
-      eats: {
-        eatsInsects: false,
-        eatsDung: false,
-        eatsPlants: true,
-      }
+  },
+  {
+    name: 'Tortoise Beetle',
+    img: 'tortoise-beetle.jpg',
+    family: 'Cassidinae',
+    size: '1-18',
+    eats: {
+      eatsInsects: false,
+      eatsDung: false,
+      eatsPlants: true,
+    }
   },
 ];
 
